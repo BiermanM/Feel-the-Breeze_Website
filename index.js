0fcee6697432bdc7f79da54abba05445b8119f49
Matthew Bierman <matthew.bierman2@gmail.com>
2018-03-30 22:28:47 -0500

    Completed basic design and layout of website

---
+++ index.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+    $('#gallery-carousel').slick({
+        dots: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        responsive: [
+            {
+                breakpoint: 900,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    dots: false
+                }
+            }
+        ]
+    });
+});
+
+$(window).resize(function() {
+    if ($(window).width() < 768)
+        $("#about-img").height("300px");
+});
+
+$(function () {
+  $('[data-toggle="popover"]').popover()
+})
