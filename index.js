af2a88873f59c2ade580ac4a8fdb427d7764e76f
Matthew Bierman <matthew.bierman2@gmail.com>
2018-04-09 18:04:23 -0500

    Fixed sizing issues for Product section and titles on extra-small screen sizes, fix Mission Statement text size for small screens

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
+// Enable scrollspy
 $('body').scrollspy({target: "#navbar-scrollspy", offset: 77});
 
+// Smooth scrolling for navbar links
 $(".nav-link").on('click', function(e) {
-
-    // prevent default anchor click behavior
     e.preventDefault();
-
-    // store hash
     var hash = this.hash;
 
-    // animate
     $('html, body').animate({
         scrollTop: ($(hash).offset().top)
-    }, 800, function(){
-        // when done, add hash to url (default click behaviour)
+    }, 800, function() {
         window.location.hash = hash;
     });
 });
 
+// Enlarge down arrow in jumbotron on hover
 $("#link-down-icon").hover(function() {
     $("#link-down-icon").transition({scale: 1.3});
 }, function() {
     $("#link-down-icon").transition({scale: 1});
 });
 
+// Smooth scrolling for down arrow in jumbotron
 $("#link-down-icon").click(function(e) {
     e.preventDefault();
     
@@ -31,7 +29,35 @@ $("#link-down-icon").click(function(e) {
     }, 800);
 });
 
+
+$(window).resize(function() {
+    fixProduct();
+});
+
+function fixProduct() {
+    // Resize product div (accordian overflows on small/medium sized screen)
+    if ($(window).width() > 575)
+        $("#product").height($("#product-title").height() + $("#product-accordion-card-1").height() + $("#product-accordion-card-2").height() + $("#product-accordion-card-3").height());
+    else
+        $("#product").height($("#product-title").height() + $(".product-img").height() + $("#product-accordion-card-1").height() + $("#product-accordion-card-2").height() + $("#product-accordion-card-3").height());
+    
+    // Make product images smaller for extra-small screens
+    if ($(window).width() < 330)
+        $(".product-img").height(200);
+    else
+        $(".product-img").height(400);
+    
+    // Make titles smaller for extra-small screens
+    if ($(window).width() < 330)
+        $(".title-text").css("font-size", 30);
+    else
+        $(".title-text").css("font-size", 50);
+}
+
+// Enable gallery
 $(document).ready(function() {
+    fixProduct();
+    
     $('#gallery-carousel').slick({
         dots: true,
         slidesToShow: 3,
@@ -39,6 +65,7 @@ $(document).ready(function() {
         autoplay: true,
         autoplaySpeed: 5000,
         responsive: [
+            // Shows 3 images at a time for large screens, 2 for medium, and 1 for small/mobile (and without dots at bottom for small/mobile)
             {
                 breakpoint: 900,
                 settings: {
@@ -56,6 +83,7 @@ $(document).ready(function() {
     });
 });
 
+// Enable popovers
 $(function () {
   $('[data-toggle="popover"]').popover()
 })
